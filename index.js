b083b9efca59dc3af28dc3e0cd3d6e1aa85d561e
jrandmaa <jrandmaa@berkeley.edu>
2020-09-29 10:58:54 -0700

    hitsounds

--- index.js
+++ index.js
@@ -5,52 +5,78 @@ const lineColor = [0, 0, 0];
 const activeLineColor = [190, 20, 110];
 const lineWidth = 3;
 const activelineWidth = 9;
-const sounds = Array.from({ length: 6 });
+const sounds = Array.from({ length: 4 });
 
 const ball1 = {
     x: 300,
-    y: 300,
-    size: 100,
-    speed: 1,
+    y: 100,
+    size: 110,
+    speed: 0.5,
     fillColor: [190,80,230],
     strokeColor: [0,220,20],
     ballStrokeWeight: 2,
-    rightSound: sounds[0],
-    leftSound: sounds[1],
-    soundLength: 2000,
+    
 } 
 
 const ball2 = {
     x: 300,
-    y: 100,
-    size: 50,
-    speed: 2,
-    fillColor: [190,80,230],
-    strokeColor: [0,220,20],
-    ballStrokeWeight: 2,
-    rightSound: sounds[2],
-    leftSound: sounds[3],
-    soundLength: 1000,
-} 
-
-const ball3 = {
-    x: 300,
-    y: 200,
+    y: 300,
     size: 80,
     speed: 2,
     fillColor: [190,80,230],
     strokeColor: [0,220,20],
     ballStrokeWeight: 2,
-    rightSound: sounds[4],
-    leftSound: sounds[5],
-    soundLength: 500,
+
 } 
+
+const ball3 = {
+    x: 300,
+    y: 450,
+    size: 60,
+    speed: 2.7,
+    fillColor: [190,80,230],
+    strokeColor: [0,220,20],
+    ballStrokeWeight: 2,
+    /*rightSound: sounds[4],
+    leftSound: sounds[5],
+    soundLength: 500,*/
+} 
+
+const ball4 = {
+    x: 300,
+    y: 550,
+    size: 50,
+    speed: 3.5,
+    fillColor: [190,80,230],
+    strokeColor: [0,220,20],
+    ballStrokeWeight: 2,
+}
 
 const leftEdge = {
     x1: 10,
     y1: 0,
     x2: 10,
     y2: 600,
+    color: lineColor,
+    width: lineWidth,
+
+}
+
+const bottomEdge = {
+    x1: 10,
+    y1: 10,
+    x2: 570,
+    y2: 10,
+    color: lineColor,
+    width: lineWidth,
+
+}
+
+const topEdge = {
+    x1: 10,
+    y1: 580,
+    x2: 570,
+    y2: 580,
     color: lineColor,
     width: lineWidth,
 
@@ -66,24 +92,24 @@ const rightEdge = {
 }
 
 
-const balls = [ball1, ball2, ball3];
+const balls = [ball1, ball2, ball3];//ball4
 
 
 
 function preload(){
 
     sounds.forEach((sound, i) => {
-        sounds[i] = loadSound(`sounds/${i}.mp3`)
+        sounds[i] = loadSound(`sounds/hit${i+1}.mp3`)
     })
 
     console.log(sounds);
 
-    ball1.rightSound = sounds[0];
+    /*ball1.rightSound = sounds[0];
     ball1.leftSound = sounds[1];
     ball2.rightSound = sounds[2];
     ball2.leftSound = sounds[3];
     ball3.rightSound = sounds[4];
-    ball3.leftSound = sounds[5];
+    ball3.leftSound = sounds[5];*/
 
     // for(let i = 0; i < sounds.length; i++){
     //     sounds[i] = loadSound(`sounds/${i}.mp3`)
@@ -105,25 +131,78 @@ function draw(){
         updateBall(ball);
         displayBall(ball);
     })
+  
     drawLine(leftEdge);
     drawLine(rightEdge);
+  
+    drawLine(topEdge);
+    drawLine(bottomEdge);
 }
 
 
 function updateBall(ball){
     console.log(ball.x);
-    if(ball.x + ball.size/2 > rightEdge.x1 ){
+    if(ball.y + ball.size/2 > topEdge.y1 ){
         ball.speed *= -1;
-        ball.rightSound.play();
-        activateLine(rightEdge);
-    } else if(ball.x - ball.size/2 < leftEdge.x1 ){
+        //ball.rightSound.play();
+        activateLine(topEdge);
+    } else if(ball.y - ball.size/2 < bottomEdge.y1 ){
         ball.speed *= -1;
-        ball.leftSound.play();
-        activateLine(leftEdge);
+        //ball.leftSound.play();
+        activateLine(bottomEdge);
+    } /*else if(ball.y - ball.size/2 < ball2.y + ball2.size/2){
+      if(ball.y + ball.size/2 >= ball2.y - ball2.size/2){
+        if(sign(ball1.speed) != sign(ball2.speed)){
+          temp = ball2.speed;
+          ball2.speed = ball.speed;
+          ball.speed = temp;
+        }
+
+      }
+    }*/ 
+  
+  balls.forEach((ball2) => {
+    if(ball2 != ball){
+      if(ball.y > ball2.y){ //above ball2
+      if(ball.y - ball.size/2 <= ball2.y + ball2.size/2){
+        /*if(sign(ball1.speed) != sign(ball2.speed)){
+          temp = ball2.speed;
+          ball2.speed = ball.speed;
+          ball.speed = temp;
+          activateLine(rightEdge);
+        }*/
+        temp = ball2.speed;
+          ball2.speed = ball.speed;
+          ball.speed = temp;
+          hitSound();
+
+      }
+    } else if(ball.y < ball2.y){ //below ball2  //smaller on top
+      if(ball.y + ball.size/2 >= ball2.y - ball2.size/2){
+        temp = ball2.speed;
+          ball2.speed = ball.speed;
+          ball.speed = temp;
+          hitSound();
+      }
     }
-    ball.x+= ball.speed;
+    }
+  })
+  
+  
+    ball.y+= ball.speed;
 }
 
+function sign(speed){
+ if(speed>0){
+   return 1;
+ } else {
+   return 0;
+ }
+}
+
+function hitSound(){
+  sounds[Math.floor(Math.random() * sounds.length)].play();
+}
 
 const displayBall = ({x, y, size, strokeColor, fillColor, ballStrokeWeight}) => {
         stroke(strokeColor);
